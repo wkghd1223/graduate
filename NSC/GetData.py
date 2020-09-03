ff54a471ff5fd7eb92351a6541ead04dbc0a16a3
Ukjan-Baek <peobin297@gmail.com>
2020-09-03 14:27:06 +0900

    fix

--- NSC/GetData.py
+++ NSC/GetData.py
@@ -1,5 +1,12 @@
 import pandas as pd
-import requests
+import plotly.offline as offline
+import plotly.graph_objs as go
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
 
 # 한국거래소(krx)에서 주식시장에 상장된 기업들에 대해 종목코드 가져오기
 code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
@@ -7,7 +14,7 @@ code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=dow
 # 종목코드가 6자리이기 때문에 6자리 맞춰주는 설정
 code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
 
-# 필요한 회사명, 종목코드 컬럼이외 필요없는 컬럽제외
+# 필요한 회사명, 종목코드 컬럼이외 필요없는 컬럼제외
 code_df = code_df[['회사명', '종목코드']]
 
 # 한글로된 컬럼명을 영어로 바꾸기
@@ -15,7 +22,6 @@ code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
 
 # 상위 5개의 코드 확인
 code_df.head()
-
 
 # 종목이름 입력하면 종목에 해당하는 코드를 불러와 네이버 금융(http://finance.naver.com)에 넣어줌
 def get_url(item_name, code_df):
@@ -26,8 +32,7 @@ def get_url(item_name, code_df):
     print("요청 URL = {}".format(url))
     return url
 
-
-# 신라젠의 일자데이터 url 가져오기
+# 종목이름의 일자데이터 url 가져오기
 item_name = '삼성전자'
 url = get_url(item_name, code_df)
 
@@ -45,3 +50,52 @@ df = df.dropna()
 
 # 상위 5개 데이터 확인하기
 df.head()
+
+df = df.rename(columns={
+    '날짜': 'date', '종가': 'close', '전일비': 'diff', '시가': 'open', '고가': 'high', '저가': 'low', '거래량': 'volume'
+})
+df[['close', 'diff', 'open', 'high', 'low', 'volume']] \
+    = df[['close', 'diff', 'open', 'high', 'low', 'volume']].astype(int)
+df['date'] = pd.to_datetime(df['date'])
+df = df.sort_values(by=['date'], ascending=True)
+df.head()
+
+# offline.init_notebook_mode(connected=True)
+
+trace = go.Scatter(
+    x=df.date,
+    y=df.close,
+    name=item_name
+)
+data = [trace]
+
+layout = dict(
+    title='{}의 종가(close) Time Series'.format(item_name),
+    xaxis=dict(
+        rangeselector = dict(
+            buttons=list([
+                dict(count=1,
+                     label='1m',
+                     step='month',
+                     stepmode='backward'
+                     ),
+                dict(count=3,
+                     label='3m',
+                     step='month',
+                     stepmode='backward'
+                     ),
+                dict(count=6,
+                     label='6m',
+                     step='month',
+                     stepmode='backward'
+                     ),
+                dict(step='all'
+                     ),
+            ])
+        ),
+        rangeslider=dict(),
+        type='date'
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+offline.plot(fig)
--- NSC/NewsCrawler.py
+++ NSC/NewsCrawler.py
@@ -8,11 +8,13 @@ from datetime import datetime
 - 네이버 뉴스만 가져와서 결과값 조금 작음 
 - 결과 메모장 저장 -> 엑셀로 저장 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-RESULT_PATH = 'C:/test/' #메모장 및 엑셀 파일 저장 경로
+# 메모장 및 엑셀 파일 저장 경로
+RESULT_PATH = 'C:/Users/USER/Desktop/백욱진/졸프_주분투/graduate/NSC/crawledNews/'
 now = datetime.now()  # 파일이름 현 시간으로 저장하기
 
+
 def get_news(n_url):
-    news_detail = [] #crawler 함수로부터 n_url을 받아서 크롤링해 리턴할 list형태의 데이터
+    news_detail = []  # crawler 함수로부터 n_url을 받아서 크롤링해 리턴할 list형태의 데이터
 
     breq = requests.get(n_url)
     bsoup = BeautifulSoup(breq.content, 'html.parser')
@@ -34,26 +36,30 @@ def get_news(n_url):
 
     return news_detail
 
+
 class crawling:
     def __init__(self, maxpage, query, s_date, e_date):
         self.maxpage = maxpage
         self.query = query
         self.s_date = s_date
         self.e_date = e_date
+
     def crawler(self, maxpage, query, s_date, e_date):
 
-        #네이버 뉴스 홈에 등록된 기사인지 확인후 맞으면 해당 url을 get_news에 보내고 리턴값(리스트 형식)을 메모장으로 저장
-        s_from = self.s_date.replace(".", "")#우측부터 "."을 찾아서 ""으로 바꿈
-        e_to = self.e_date.replace(".", "")#우측부터 "."을 찾아서 ""으로 바꿈
+        # 네이버 뉴스 홈에 등록된 기사인지 확인후 맞으면 해당 url을 get_news에 보내고 리턴값(리스트 형식)을 메모장으로 저장
+        s_from = self.s_date.replace(".", "")  # 우측부터 "."을 찾아서 ""으로 바꿈
+        e_to = self.e_date.replace(".", "")  # 우측부터 "."을 찾아서 ""으로 바꿈
         page = 1
         maxpage_t = (int(self.maxpage) - 1) * 10 + 1  # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
-        f = open("C:/test/crawling_result.txt", 'w', encoding='utf-8')#open(파일 이름, 파일 열기모드 w=쓰기모드)
+        f = open("C:/Users/USER/Desktop/백욱진/졸프_주분투/graduate/NSC/crawledNews/crawling_result.txt", 'w',
+                 encoding='utf-8')  # open(파일 이름, 파일 열기모드 w=쓰기모드)
 
-        while page < maxpage_t:#page가 maxpage_t보다 작을 동안
+        while page < maxpage_t:  # page가 maxpage_t보다 작을 동안
 
             print(page)
 
-            url = "https://search.naver.com/search.naver?where=news&query=" + self.query + "&sort=0&ds=" + self.s_date + "&de=" + self.e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
+            url = "https://search.naver.com/search.naver?where=news&query=" + self.query + "&sort=0&ds=" + self.s_date + "&de=" + self.e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(
+                page)
 
             req = requests.get(url)
             print(url)
@@ -61,14 +67,15 @@ class crawling:
             soup = BeautifulSoup(cont, 'html.parser')
             # print(soup)
 
-            for urls in soup.select("._sp_each_url"):# '_sp_each_url'은 네이버뉴스로 가는 하이퍼링크가 걸린 부분의 클래스명
+            for urls in soup.select("._sp_each_url"):  # '_sp_each_url'은 네이버뉴스로 가는 하이퍼링크가 걸린 부분의 클래스명
                 try:
                     # print(urls["href"])
                     if urls["href"].startswith("https://news.naver.com"):
                         # print(urls["href"])
                         news_detail = get_news(urls["href"])
                         # pdate, pcompany, title, btext
-                        f.write("{}\t{}\t{}\t{}\t{}\n".format(news_detail[1], news_detail[4], news_detail[0], news_detail[2],news_detail[3]))  # new style
+                        f.write("{}\t{}\t{}\t{}\t{}\n".format(news_detail[1], news_detail[4], news_detail[0],
+                                                              news_detail[2], news_detail[3]))  # new style
                 except Exception as e:
                     print(e)
                     continue
@@ -76,25 +83,25 @@ class crawling:
 
         f.close()
 
-    # def excel_make():
-    #     data = pd.read_csv(RESULT_PATH + 'crawling_result.txt', sep='\t', header=None, error_bad_lines=False)
-    #     data.columns = ['years', 'company', 'title', 'contents', 'link']
-    #     print(data)
-    #
-    #     xlsx_outputFileName = '%s-%s-%s  %s시 %s분 %s초 result.xlsx' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
-    #     #xlsx_name = 'result' + '.xlsx'
-    #     data.to_excel(RESULT_PATH + xlsx_outputFileName, encoding='utf-8')
+    def excel_make(self):
+        data = pd.read_csv(RESULT_PATH + 'crawling_result.txt', sep='\t', header=None, error_bad_lines=False)
+        data.columns = ['years', 'company', 'title', 'contents', 'link']
+        print(data)
 
-def main(): #사용자로부터 입력 값 받아서 crawler함수 넘겨주고 엑셀로 만들라는 지시
+        xlsx_outputFileName = '%s-%s-%s  %s시 %s분 %s초 result.xlsx' % (
+            now.year, now.month, now.day, now.hour, now.minute, now.second)
+        xlsx_name = 'result' + '.xlsx'
+        data.to_excel(RESULT_PATH + xlsx_outputFileName, encoding='utf-8')
+
+
+def main():  # 사용자로부터 입력 값 받아서 crawler함수 넘겨주고 엑셀로 만들라는 지시
     maxpage = input("최대 출력할 페이지수 입력하시오: ")
     query = input("검색어 입력: ")
     s_date = input("시작날짜 입력(2019.01.01):")  # 2019.01.01
     e_date = input("끝날짜 입력(2019.04.28):")  # 2019.04.28
     c = crawling(maxpage, query, s_date, e_date)
     c.crawler(maxpage, query, s_date, e_date)  # 검색된 네이버뉴스의 기사내용을 크롤링합니다.
+    c.excel_make()  # 엑셀로 만들기
 
-    # c.excel_make()  # 엑셀로 만들기
 
 main()
-
-
