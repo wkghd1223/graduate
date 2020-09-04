02429059114988d07f6afb5a901783270f2b52c2
Ukjan-Baek <peobin297@gmail.com>
2020-09-04 15:14:23 +0900

    #11 뉴스검색기능 구현

--- NSC/NewsCrawler.py
+++ NSC/NewsCrawler.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
-from openpyxl.workbook import Workbook
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 < naver 뉴스 전문 가져오기 >_select 사용
@@ -21,11 +20,14 @@ def get_news(n_url):
     breq = requests.get(n_url)
     bsoup = BeautifulSoup(breq.content, 'html.parser')
 
+    pdate = bsoup.select('.t11')[0].get_text()[:11]
+    news_detail.append(pdate)
+
     title = bsoup.select('h3#articleTitle')[0].text  # 대괄호는  h3#articleTitle 인 것중 첫번째 그룹만 가져오겠다.
     news_detail.append(title)
 
-    pdate = bsoup.select('.t11')[0].get_text()[:11]
-    news_detail.append(pdate)
+    pcompany = bsoup.select('#footer address')[0].a.get_text()
+    news_detail.append(pcompany)
 
     # _text = bsoup.select('#articleBodyContents')[0].get_text().replace('\n', " ")
     # btext = _text.replace("// flash 오류를 우회하기 위한 함수 추가 function _flash_removeCallback() {}", "")
@@ -33,8 +35,6 @@ def get_news(n_url):
 
     news_detail.append(n_url)
 
-    pcompany = bsoup.select('#footer address')[0].a.get_text()
-    news_detail.append(pcompany)
 
     return news_detail
 
@@ -47,10 +47,10 @@ class crawling:
         self.e_date = e_date
 
     def crawler(self, maxpage, query, s_date, e_date):
-
+        result = [];
         # 네이버 뉴스 홈에 등록된 기사인지 확인후 맞으면 해당 url을 get_news에 보내고 리턴값(리스트 형식)을 메모장으로 저장
-        s_from = self.s_date.replace(".", "")  # 우측부터 "."을 찾아서 ""으로 바꿈
-        e_to = self.e_date.replace(".", "")  # 우측부터 "."을 찾아서 ""으로 바꿈
+        s_from = self.s_date.replace("-", "")  # 우측부터 "."을 찾아서 ""으로 바꿈
+        e_to = self.e_date.replace("-", "")  # 우측부터 "."을 찾아서 ""으로 바꿈
         page = 1
         maxpage_t = (int(self.maxpage) - 1) * 10 + 1  # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
 
@@ -76,19 +76,22 @@ class crawling:
                     if urls["href"].startswith("https://news.naver.com"):
                         # print(urls["href"])
                         news_detail = get_news(urls["href"])
-                        print("1. title : "+news_detail[0])
-                        print("2. pdate : "+news_detail[1])
-                        print("3. url : "+news_detail[2])
-                        print("4. company : "+news_detail[3])
+                        # print("1. title : "+news_detail[0])
+                        # print("2. pdate : "+news_detail[1])
+                        # print("3. url : "+news_detail[2])
+                        # print("4. company : "+news_detail[3])
 
                         # pdate, pcompany, title, btext
                         # f.write("{}\t{}\t{}\t{}\t{}\n".format(news_detail[1], news_detail[4], news_detail[0],
                         #                                       news_detail[2], news_detail[3]))  # new style
+                        result.append(news_detail)
+
                 except Exception as e:
                     print(e)
                     continue
             page += 10
 
+        return result
         # f.close()
 
     # def excel_make(self):
@@ -102,18 +105,17 @@ class crawling:
     #     data.to_excel(RESULT_PATH + xlsx_outputFileName, encoding='utf-8')
 
 
-def main():  # 사용자로부터 입력 값 받아서 crawler함수 넘겨주고 엑셀로 만들라는 지시
-    # maxpage = input("최대 출력할 페이지수 입력하시오: ")
-    # query = input("검색어 입력: ")
-    # s_date = input("시작날짜 입력(2019.01.01):")  # 2019.01.01
-    # e_date = input("끝날짜 입력(2019.04.28):")  # 2019.04.28
-    maxpage = 5
-    query = '삼성전자우'
-    s_date = '2019.01.01'
-    e_date = '2019.04.28'
-    c = crawling(maxpage, query, s_date, e_date)
-    c.crawler(maxpage, query, s_date, e_date)  # 검색된 네이버뉴스의 기사내용을 크롤링합니다.
-    # c.excel_make()  # 엑셀로 만들기
+# def main():  # 사용자로부터 입력 값 받아서 crawler함수 넘겨주고 엑셀로 만들라는 지시
+#     # maxpage = input("최대 출력할 페이지수 입력하시오: ")
+#     # query = input("검색어 입력: ")
+#     # s_date = input("시작날짜 입력(2019.01.01):")  # 2019.01.01
+#     # e_date = input("끝날짜 입력(2019.04.28):")  # 2019.04.28
+#     crawledNews=[]
+#     maxpage = 5
+#     query = '삼성전자우'
+#     s_date = '2019.01.01'
+#     e_date = '2020.04.28'
+#     c = crawling(maxpage, query, s_date, e_date)
+#     crawledNews = c.crawler(maxpage, query, s_date, e_date)  # 검색된 네이버뉴스의 기사내용을 크롤링합니다.
+#     # c.excel_make()  # 엑셀로 만들기
 
-
-main()
--- NSC/news.py
+++ NSC/news.py
@@ -3,10 +3,43 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
-from NewsCrawler import *
+from NewsCrawler import crawling
+from datetime import date, timedelta
 
 
-class NewsWindowClass(QmainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+
+class NewsWindow(QMainWindow):
+    def __init__(self, parent):
+        super(NewsWindow, self).__init__(parent)
+        ui = "ui/news.ui"
+        uic.loadUi(ui, self)
+
+        self.show()
+        self.setTable()
+        self.searchBtn.clicked.connect(self.getNews)
+        print(date.today())
+    def getNews(self):
+        today = datetime.date.today()
+
+        maxpage = 2
+        query = self.paramForSearch.text()
+        s_date = datetime.date.today() - datetime.date.timedelta(days = -5000)
+        e_date = datetime.date.today()
+        c = crawling(maxpage, query, s_date, e_date)
+        result = c.crawler(maxpage, query, s_date, e_date)
+        print(s_date)
+
+        for i in range(len(result)):
+            for j in range(len(result[i])):
+                self.crawledResult.setItem(i,j,QTableWidgetItem(result[i][j]))
+
+
+    def setTable(self):
+
+        self.crawledResult.setColumnCount(4)
+        self.crawledResult.setRowCount(99)
+        self.crawledResult.setHorizontalHeaderLabels(["날짜", "제목", "회사", "URL"])
+
+
+
+
